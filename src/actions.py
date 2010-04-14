046a75e2a99661ad585a6fd4f1c4856fb41f9240
Jan <laanjj@gmail.com>
2010-04-14 11:13:52 +0200

    actions enzo

--- src/actions.py
+++ src/actions.py
@@ -3,76 +3,75 @@ from packedmessage import *
 
 
 
-def handle_100():
+def handle_100(message, address, client):
   pass
   
-def handle_110():
-  for 
+def handle_110(message, address, client):
   pass
 
-def handle_120():
+def handle_120(message, address, client):
   pass
 
-def handle_130():
+def handle_130(message, address, client):
   pass
 
-def handle_140():
-  control.send(MessagePacker(150, self.message).get)
+def handle_140(message, address, client):
+  client.send(MessagePacker(150, message).get())
 
-def handle_150():
+def handle_150(message, address, client):
   pass
 
-def handle_160():
+def handle_160(message, address, client):
   pass
 
-def handle_170():
+def handle_170(message, address, client):
   pass
 
-def handle_200():
+def handle_200(message, address, client):
   pass
 
-def handle_210():
+def handle_210(message, address, client):
   pass
 
-def handle_300():
+def handle_300(message, address, client):
   pass
 
-def handle_310():
+def handle_310(message, address, client):
   pass
 
 
-def handle_500():
+def handle_500(message, address, client):
   pass
 
-def handle_510():
+def handle_510(message, address, client):
   pass
 
-def handle_520():
+def handle_520(message, address, client):
   pass
 
-def handle_530():
+def handle_530(message, address, client):
   pass
 
-def handle_600():
+def handle_600(message, address, client):
   pass
 
-def handle_601():
+def handle_601(message, address, client):
   pass
 
-def handle_602():
+def handle_602(message, addres, client):
   pass
 
-def handle_603():
+def handle_603(message, address, client):
   pass
 
-def handle_604():
+def handle_604(message, address, client):
   pass
 
-def handle_610():
+def handle_610(message, address, client):
   pass
 
-def handle_611():
+def handle_611(message, address, client):
   pass
 
-def handle_700():
+def handle_700(message, address, client):
   pass
