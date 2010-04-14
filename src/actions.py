4248ceca8745d75c30235da75d221f9c32c5fa4a
Bas <insanecow1988@gmail.com>
2010-04-14 11:06:11 +0200

    actions.py

--- src/actions.py
+++ src/actions.py
@@ -1,2 +1,78 @@
-def handlePing(la, la, la):
-   pass
+from connection import *
+from packedmessage import *
+
+
+
+def handle_100():
+  pass
+  
+def handle_110():
+  for 
+  pass
+
+def handle_120():
+  pass
+
+def handle_130():
+  pass
+
+def handle_140():
+  control.send(MessagePacker(150, self.message).get)
+
+def handle_150():
+  pass
+
+def handle_160():
+  pass
+
+def handle_170():
+  pass
+
+def handle_200():
+  pass
+
+def handle_210():
+  pass
+
+def handle_300():
+  pass
+
+def handle_310():
+  pass
+
+
+def handle_500():
+  pass
+
+def handle_510():
+  pass
+
+def handle_520():
+  pass
+
+def handle_530():
+  pass
+
+def handle_600():
+  pass
+
+def handle_601():
+  pass
+
+def handle_602():
+  pass
+
+def handle_603():
+  pass
+
+def handle_604():
+  pass
+
+def handle_610():
+  pass
+
+def handle_611():
+  pass
+
+def handle_700():
+  pass
