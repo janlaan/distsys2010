1f9f0a17db26581a141ad9bfd03f6e27046d517c
rav <rgopal@science.uva.nl>
2010-04-21 12:59:06 +0200

    logs worden dynamisch aangemaakt

--- src/log.py
+++ src/log.py
@@ -4,10 +4,12 @@ Each module logs to it's own file.
 
 Usage:
 in module <name>:
-    import logging, log
+   import logging, log
 
-    logger = logging.getLogger('name')
-    logger.info('This is example info') 
+   logInstance = log.logger('name')
+   logger = logging.getLogger('name')
+   log.logger.init(logInstance, logger)
+   logger.info('This is example info')
 
 Author  : Ravish
 Date    : 11-4-2010
@@ -18,43 +20,20 @@ import logging, logging.handlers
 
 class logger:
 
-    # Set up logging variables
-    logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)2s %(module)-12s %(levelname)-4s %(message)s',
-                    datefmt='%d-%m %H:%M')
-    datefmt = '%d-%m %H:%M'
-    format = logging.Formatter('%(asctime)2s %(module)-2s %(levelname)-4s %(message)s', datefmt)
+   # Set up logging variables
+   logging.basicConfig(level=logging.DEBUG,
+                   format='%(asctime)2s %(module)-12s %(levelname)-4s %(message)s',
+                   datefmt='%d-%m %H:%M')
+   datefmt = '%d-%m %H:%M'
+   format = logging.Formatter('%(asctime)2s %(module)-2s %(levelname)-4s %(message)s', datefmt)
 
-    # Server action log
-    sLogger = logging.getLogger('serveraction')
-    sFile = logging.FileHandler('../log/serveraction.log')
-    sFile.setFormatter(format)
-    sLogger.addHandler(sFile)
+   def __init__(self, name):
+      self.name = name
 
-    # Connection log
-    cLogger = logging.getLogger('connection')
-    cFile = logging.FileHandler('../log/connection.log')
-    cFile.setFormatter(format)
-    cLogger.addHandler(cFile)
-
-    # Actions log
-    aLogger = logging.getLogger('action')
-    aFile = logging.FileHandler('../log/action.log')
-    aFile.setFormatter(format)
-    aLogger.addHandler(aFile)
-
-    # Listener log
-    lLogger = logging.getLogger('listener')
-    lFile = logging.FileHandler('../log/listener.log')
-    lFile.setFormatter(format)
-    lLogger.addHandler(lFile)
-
-    # Database log
-    dLogger = logging.getLogger('database')
-    dFile = logging.FileHandler('../log/database.log')
-    dFile.setFormatter(format)
-    dLogger.addHandler(dFile)
+   def init(self, Logger):
+      File = logging.FileHandler('../log/' + self.name + '.log')
+      File.setFormatter(self.format)
+      Logger.addHandler(File)
 
 if __name__ == '__main__':
-    logging.info('This is info')
-
+   logging.info('This is info')
